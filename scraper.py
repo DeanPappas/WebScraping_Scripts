59ac7e7964bba3c06ad6efeca3dcc5b3efec3142
Dean Pappas <kpappas302@gmail.com>
2019-09-12 14:50:37 -0500

    Sale Scraper Complete; currently checks every 3 hours

--- scraper.py
+++ scraper.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import smtplib
+import time
 
 URL = "https://www.amazon.com/Wireless-Bluetooth-Audio-Technica-ATH-M50X-Headphones/dp/B07HNP7RZH"
 
@@ -16,12 +17,36 @@ title = soup2.find(id = "productTitle").get_text()
 price = soup2.find(id = "priceblock_ourprice").get_text()
 converted_price = float(price[1:6])
 
-if converted_price < 39.95:
-	send_email()
-		
 print(title.strip())
 print(price.strip())
 
+def check_price():
+	if converted_price < 39.95:
+		send_email()
+	else:
+		print("No Sale")
+		
+
 def send_email():
+	server = smtplib.SMTP("smtp.gmail.com", 587)
+	server.ehlo()
+	server.starttls()
+	server.ehlo()
+	
+	server.login("kpappas302@gmail.com", "pyxdhxaglwvndtni")
+	
+	subject = "~AudioTechnica BT Adapter Price Drop~"
+	body = "https://www.amazon.com/Wireless-Bluetooth-Audio-Technica-ATH-M50X-Headphones/dp/B07HNP7RZH"
+	
+	msg = ("Subject: %s\n\n%s") % (subject, body)
+	
+	server.sendmail("kpappas302@gmail.com","kpappas302@gmail.com",msg)
+	print("EMAIL SENT")
+	
+	server.quit()
+	
+while(True):	
+	check_price()
+	time.sleep(10800)
 	
 #https://www.youtube.com/watch?v=Bg9r_yLk7VY AT 11:13   pw: pyxd hxag lwvn dtni
